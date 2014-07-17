e5426546cc428f7d7ecf5b6d8745350668921bd2
Kaleb Lape <pingortle@gmail.com>
2014-07-17 10:41:46 -0500

    Add initial schemas.

--- collections/collections.js
+++ collections/collections.js
@@ -1,7 +1,52 @@
+AppSchema = {};
+
+AppSchema.Workflow = new SimpleSchema({
+	slug: {
+		type: String,
+		label: "Unique Name",
+		regEx: /^[a-z0-9-]{2,64}$/,
+		index: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		label: "Displayed Name",
+		max: 100,
+	},
+	actionLinks: {
+		type: [Object],
+		maxCount: 10,
+		optional: true,
+	},
+	"actionLinks.$.link": {
+		type: String,
+		label: "Action Link",
+		max: 200,
+	},
+	"actionLinks.$.button_label": {
+		type: String,
+		label: "Button Label",
+		max: 140,
+	},
+});
+
 Workflows = new Meteor.Collection('workflows');
-if (Meteor.isServer)
-	Workflows._ensureIndex("slug", { unique: 1 });
+Workflows.attachSchema(AppSchema.Workflow);
+
+AppSchema.Project = new SimpleSchema({
+	wbs: {
+		type: String,
+		label: "WBS",
+		regEx: /^[0-9]{3}\.[0-9]{3}$/,
+		index: true,
+		unique: true,
+	},
+	description: {
+		type: String,
+		label: "Description",
+		max: 100,
+	},
+});
 
 Projects = new Meteor.Collection('projects');
-if (Meteor.isServer)
-	Projects._ensureIndex("wbs", { unique: 1 });
+Projects.attachSchema(AppSchema.Project);
