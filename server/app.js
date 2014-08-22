bbe5a2f36304b3e9cd6f5e47ba344eeb2110d2e5
Kaleb Lape <pingortle@gmail.com>
2014-08-22 16:01:30 -0500

    Add status to fixture data.

--- server/app.js
+++ server/app.js
@@ -60,18 +60,22 @@ Meteor.startup(function() {
 			{
 				wbs: "010.017",
 				description: "Southwest PowerPool Ops Center/Parking Garage",
+				status: "sold",
 			},
 			{
 				wbs: "012.002",
 				description: "Valley View Gym",
+				status: "sold",
 			},
 			{
 				wbs: "012.018",
 				description: "Calico Rock Addition",
+				status: "sold",
 			},
 			{
 				wbs: "012.014",
 				description: "Brookland High School Addition",
+				status: "sold",
 			},
 		];
 		_.each(projects, function(x) {
