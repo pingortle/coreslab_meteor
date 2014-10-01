849010d7fde3f5d49b8136ec1b8a4ffe3bca0163
Kaleb Lape <pingortle@gmail.com>
2014-10-01 13:35:13 -0500

    Enable deleting beds.

--- client/views/beds/beds.js
+++ client/views/beds/beds.js
@@ -3,4 +3,8 @@ Template.beds.events({
     var id = e.currentTarget.dataset.itemId;
     Router.go('edit_bed', {id: id});
   },
+  'click button.remove-item': function (e) {
+    var id = e.currentTarget.dataset.itemId;
+    Beds.remove(id);
+  },
 });
