58bfae10b7c79e319116df2af89eade5b9999602
Kaleb Lape <pingortle@gmail.com>
2014-10-01 15:40:33 -0500

    Remove seed data.

--- server/app.js
+++ server/app.js
@@ -33,10 +33,6 @@ Meteor.methods({
 	}
 });
 
-var logDatabaseSeed = function(collection) {
-	console.log("Seeding " + collection._name + "...");
-};
-
 // Docs @ http://docs.meteor.com/#meteor_startup
 Meteor.startup(function() {
 	if (Roles.getAllRoles().count() === 0) {
@@ -60,14 +56,6 @@ Meteor.startup(function() {
 					{
 						link: "/projects",
 						button_label: "Browse projects..."
-					},
-					{
-						link: "/do_something",
-						button_label: "Do something!"
-					},
-					{
-						link: "/do_something_else",
-						button_label: "Do something else!"
 					},
 				],
 			},
@@ -95,64 +83,6 @@ Meteor.startup(function() {
 		_.each(workflows, function(x) {
 			Roles.createRole(x.slug);
 			Workflows.insert(x);
-		});
-	}
-
-	if (Projects.find().count() === 0) {
-		logDatabaseSeed(Projects);
-		var projects = [
-			{
-				id: "010.017",
-				description: "Southwest PowerPool Ops Center/Parking Garage",
-				customer: "Jimmy Dean Baked Beans Inc.",
-				status: "sold",
-				estimatedPourCount: 10,
-				estimatedLoads: {
-					legal: 1,
-					permit: 2,
-					escort: 3,
-				},
-				elements: [],
-			},
-			{
-				id: "012.002",
-				description: "Valley View Gym",
-				status: "sold",
-				estimatedPourCount: 10,
-				estimatedLoads: {
-					legal: 1,
-					permit: 2,
-					escort: 3,
-				},
-				elements: [],
-			},
-			{
-				id: "012.018",
-				description: "Calico Rock Addition",
-				status: "sold",
-				estimatedPourCount: 4,
-				estimatedLoads: {
-					legal: 1,
-					permit: 1,
-					escort: 1,
-				},
-				elements: [],
-			},
-			{
-				id: "012.014",
-				description: "Brookland High School Addition",
-				status: "sold",
-				estimatedPourCount: 90,
-				estimatedLoads: {
-					legal: 0,
-					permit: 0,
-					escort: 0,
-				},
-				elements: [],
-			},
-		];
-		_.each(projects, function(x) {
-			Projects.insert(x);
 		});
 	}
 });
