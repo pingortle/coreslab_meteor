23c0e922cb06052b8de02615c5e2f06ec1a6e67c
Kaleb Lape <pingortle@gmail.com>
2014-10-01 13:30:19 -0500

    Enable deleting workflows.

--- client/views/workflows/workflows.js
+++ client/views/workflows/workflows.js
@@ -3,4 +3,8 @@ Template.workflows_listing.events({
     var id = e.currentTarget.dataset.itemId;
     Router.go('edit_workflow', {id: id});
   },
+  'click button.remove-item': function (e) {
+    var id = e.currentTarget.dataset.itemId;
+    Workflows.remove(id);
+  },
 });
