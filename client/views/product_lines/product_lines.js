0f42063003afb941cba50db4dabe3f05dcbd9155
Kaleb Lape <pingortle@gmail.com>
2014-10-01 13:36:38 -0500

    Enable deleting product lines.

--- client/views/product_lines/product_lines.js
+++ client/views/product_lines/product_lines.js
@@ -2,5 +2,9 @@ Template.product_lines.events({
   'click button.update-item': function (e) {
     var id = e.currentTarget.dataset.itemId;
     Router.go('edit_product_line', {id: id});
-  }
+  },
+  'click button.remove-item': function (e) {
+    var id = e.currentTarget.dataset.itemId;
+    ProductLines.remove(id);
+  },
 });
