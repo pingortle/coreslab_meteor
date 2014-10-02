5509a9816c701ab8c3ac4ec6cd70712bd38b700d
Kaleb Lape <pingortle@gmail.com>
2014-10-02 14:22:40 -0500

    Only log in users with a valid email.

--- server/accounts.js
+++ server/accounts.js
@@ -5,3 +5,13 @@ Accounts.onLogin(function(attempt) {
 	else if (!Roles.userIsInRole(attempt.user._id, ["basic"]))
 		Roles.addUsersToRoles(attempt.user._id, ["basic"]);
 });
+
+// Docs @ http://docs.meteor.com/#accounts_validateloginattempt
+Accounts.validateLoginAttempt(function(attempt) {
+  if (attempt.user && !_.some(
+      attempt.user.emails,
+      function(email) { return email.verified; }))
+    throw new Meteor.Error(403, 'Email address not verified.');
+
+  return true;
+});
