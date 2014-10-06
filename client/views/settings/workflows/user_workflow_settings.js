8f77eca089401ca025f35f356088a96b51be31d1
Kaleb Lape <pingortle@gmail.com>
2014-10-06 15:31:46 -0500

    Simplify the workflow assignment screen.

--- client/views/settings/workflows/user_workflow_settings.js
+++ client/views/settings/workflows/user_workflow_settings.js
@@ -1,29 +1,29 @@
-var userWorkflows = null;
-
 var sortBySlug = { sort: {slug: 1} };
 
 Template.user_workflow_settings.helpers({
-  userWorkflows: userWorkflows = function (userId) {
-    return isSuper(userId) ? Workflows.find({}, sortBySlug) : Workflows.find({slug: { $in: roleArray(userId) }}, sortBySlug);
+  workflows: function (userId) {
+    return Workflows.find({}, sortBySlug);
   },
-  nonUserWorkflows: function (userId) {
-    return isSuper(userId) ? [] : Workflows.find({slug: { $nin: roleArray(userId) }}, sortBySlug);
-  }
+  doesOwnWorkflow: function (userId, slug) {
+    return Roles.userIsInRole(userId, [slug]);
+  },
+  username: function(user) {
+    var emailObj = user && _.first(user.emails);
+    return user && user.username || emailObj && emailObj.address;
+  },
 });
 
 Template.user_workflow_settings.events({
-  'change select#addWorkflowToUser': function (e, t) {
-    var slug = e.target.value;
-    Meteor.call('addUserToRole', t.data.user._id, slug, function (error, result) {
-      if (error)
-        console.log(error);
-    });
-  },
-  'click button.remove-workflow': function (e, t) {
+  'click .workflow-item': function (e, t) {
     var slug = e.target.dataset.slug;
-    Meteor.call('removeUserFromRole', t.data.user._id, slug, function (error, result) {
-      if (error)
-        console.log(error);
-    });
-  }
+    if (Roles.userIsInRole(t.data.user._id, [slug])) {
+      Meteor.call('removeUserFromRole', t.data.user._id, slug, function (error, result) {
+        if (error) console.log(error);
+      });
+    } else {
+      Meteor.call('addUserToRole', t.data.user._id, slug, function (error, result) {
+        if (error) console.log(error);
+      });
+    }
+  },
 });
