d25c275fb144190941ccfc86b268aafc6f2d2372
Kaleb Lape <pingortle@gmail.com>
2014-10-15 11:37:57 -0500

    Remove old debug messages.

--- client/views/settings/workflows/new_workflow.js
+++ client/views/settings/workflows/new_workflow.js
@@ -6,11 +6,9 @@ Template.afObjectField_action_links.helpers({
         return x.keys.length === 0;
       }), function (x) { return x.name; }),
       function (x) { return { label: x.name, value: x.options.path }; });
-    console.log(res);
     return [{ label: "Please select a link...", value: "" }].concat(res);
   },
   linkField: function(element) {
-    console.log(this);
     return (element || this.atts.name) + ".link";
   },
 });
