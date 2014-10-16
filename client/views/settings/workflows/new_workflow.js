eea8696c8c3ed7c0d563dbf8bfd0243c4c411c5f
Kaleb Lape <pingortle@gmail.com>
2014-10-16 12:02:59 -0500

    Pretty up action link selection.

--- client/views/settings/workflows/new_workflow.js
+++ client/views/settings/workflows/new_workflow.js
@@ -1,11 +1,30 @@
+var routeNameToDisplay = function(name) {
+  return name.split("_").join(" ");
+};
+
+var removeWords = function(str, words) {
+  return _.reduce(words, function(memo, word) {
+    return memo.replace(word, "");
+  }, str);
+};
+
+var containsAny = function(it, these) {
+  return _.some(these, function(x) { return it.indexOf(x) > -1 });
+};
+
 Template.afObjectField_action_links.helpers({
   routes: function () {
-    var res =
-    _.map(
-      _.sortBy(_.filter(Router.routes, function (x) {
-        return x.keys.length === 0;
-      }), function (x) { return x.name; }),
-      function (x) { return { label: x.name, value: x.options.path }; });
+    var res = _.chain(Router.routes)
+      .filter(function (x) {
+          return x.keys.length === 0 && !containsAny(x.name, ["home", "settings", "workflow"]);
+        })
+      .map(function (x) {
+          return { label: routeNameToDisplay(x.name), value: x.options.path };
+        })
+      .sortBy(function (x) {
+        return removeWords(x.label, ["new "]);
+      }).value();
+
     return [{ label: "Please select a link...", value: "" }].concat(res);
   },
   linkField: function(element) {
